cff086e706f86264fa4aaa87f45e28ac08f2f5f3
Mathieu Tuli <mathieutuli@Mathieus-MacBook-Pro.local>
2018-11-18 15:14:39 -0500

    Restructure for pip packaging

--- setup.py
+++ setup.py
@@ -3,19 +3,25 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines())
+
 setuptools.setup(
-    name="tensorflow-networks",
-    version="0.0.1",
+    name="TFNetworks",
+    version="0.1",
     author="Mathieu Tuli",
     author_email="tuli.mathieu@gmail.com",
-    description="Various machine learning networks built in TensorFlow, adaptable to configuration.",
+    description="TFNetworks is an easy to use API for building various types of networks in TensorFlow.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/MathieuTuli/TFNetworks",
     packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+    install_requires=requirements,
     classifiers=[
+        "Intened Audience :: Anyone",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "License :: Other/Proprietar License",
     ],
+    scripts=glob('bin/*'),
 )
